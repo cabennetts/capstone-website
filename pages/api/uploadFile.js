de27b6733b0d6f4b0bf471fcd35f7002d4aa7c2c
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-02-09 10:43:50 -0600

    fixed build error with eslint

--- pages/api/uploadFile.js
+++ pages/api/uploadFile.js
@@ -12,50 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function main(
-    bucketName = 'my-bucket',
-    filePath = './local/path/to/file.txt',
-    destFileName = 'file.txt',
-    generationMatchPrecondition = 0
-  ) {
-    // [START storage_upload_file]
-    /**
-     * TODO(developer): Uncomment the following lines before running the sample.
-     */
-    // The ID of your GCS bucket
-    const bucketName = 'sli-website-videos';
+// function main(
+//     bucketName = 'my-bucket',
+//     filePath = './local/path/to/file.txt',
+//     destFileName = 'file.txt',
+//     generationMatchPrecondition = 0
+//   ) {
+//     // [START storage_upload_file]
+   
+//     // The ID of your GCS bucket
+//     const bucketName = 'sli-website-videos';
   
-    // The path to your file to upload
-    const filePath = '../../videos/color.mp4';
+//     // The path to your file to upload
+//     const filePath = '../../videos/color.mp4';
   
-    // The new ID for your GCS file
-    const destFileName = 'upload1';
+//     // The new ID for your GCS file
+//     const destFileName = 'upload1';
   
-    // Imports the Google Cloud client library
-    const {Storage} = require('@google-cloud/storage');
+//     // Imports the Google Cloud client library
+//     const {Storage} = require('@google-cloud/storage');
   
-    // Creates a client
-    const storage = new Storage();
+//     // Creates a client
+//     const storage = new Storage();
   
-    async function uploadFile() {
-      const options = {
-        destination: destFileName,
-        // Optional:
-        // Set a generation-match precondition to avoid potential race conditions
-        // and data corruptions. The request to upload is aborted if the object's
-        // generation number does not match your precondition. For a destination
-        // object that does not yet exist, set the ifGenerationMatch precondition to 0
-        // If the destination object already exists in your bucket, set instead a
-        // generation-match precondition using its generation number.
-        // preconditionOpts: {ifGenerationMatch: generationMatchPrecondition},
-      };
+//     async function uploadFile() {
+//       const options = {
+//         destination: destFileName,
+        
+//         // preconditionOpts: {ifGenerationMatch: generationMatchPrecondition},
+//       };
   
-      await storage.bucket(bucketName).upload(filePath, options);
-      console.log(`${filePath} uploaded to ${bucketName}`);
-    }
+//       await storage.bucket(bucketName).upload(filePath, options);
+//       console.log(`${filePath} uploaded to ${bucketName}`);
+//     }
   
-    uploadFile().catch(console.error);
-    // [END storage_upload_file]
-  }
+//     uploadFile().catch(console.error);
+//     // [END storage_upload_file]
+//   }
   
-  main(...process.argv.slice(2));
+//   main(...process.argv.slice(2));
